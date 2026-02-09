d3761f01c568da1e2395569f4e2db5dc47f9e612
Harsha <harshavardhan10003@gmail.com>
2026-02-09 15:12:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// create a counter app
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
